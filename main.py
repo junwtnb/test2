6c5b65d864b2d1587360941a3ab644c1242306a1
jun <mb.pftite.hgstoire@gmail.com>
2023-09-23 11:00:01 +0900

    add main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ st.write('wigets')
 latest_iteration = st.empty()
 bar = st.progress(0)
 
-for i in range(100):
+for i in range(10):
     latest_iteration.text(f'Iteratuon {i+1}')
     time.sleep(1)
     bar.progress(i + 1)
